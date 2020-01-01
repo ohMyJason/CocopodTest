38960005e0b96c0b86cfe5da8f4794fc6a9e5527
Jason <jason@JasondeMac.local>
2020-01-01 19:00:53 +0800

    Initial Commit

---
+++ CocoapodTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocoapodTest
+//
+//  Created by Jason on 2020/1/1.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
